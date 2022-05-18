0c291f77ee286a0ce9599d3cca3c4609b32c1613
José Pallo <jose.pallo01@epn.edu.ec>
2022-05-18 08:57:12 -0500

    Operadores 2 y Funciones

--- 01-javascript/03-operadores.js
+++ 01-javascript/03-operadores.js
@@ -78,3 +78,91 @@ const respuestaIndex = arreglo
 );
 
 console.log('respuestaIndex', respuestaIndex);
+
+// FOR EACH
+// intera el arreglo
+
+const respuestaForEach =  arreglo
+    .forEach(
+      function (valorActual, indiceActual, arregloCompleto) {
+          console.log('valorActual', valorActual);
+      }
+    );
+console.log('respuestaForEach', respuestaForEach); //Undefined
+
+
+//MAP (Modificar o MUTAR  el arreglo y devuelve un nuevo arreglo)
+//emviamos los datos del nuevo arreglo
+
+const respuestaMap =  arreglo
+    .map(
+      (valorActual, indiceInicial, arregloCompleto)=>{
+          const nuevoElemento = {
+              id: valorActual.id,
+              nombre: valorActual.nombre,
+              nota: valorActual.nota,
+              casado: false
+          };
+          return nuevoElemento;
+      }
+    );
+console.log('respuestaMap', respuestaMap);
+console.log('arreglo', arreglo);
+
+// FILTER (filtrar el arreglo)
+// envimos EXPRESION TRUTY FALSY
+// devuelve los elementos que cumplen esa condición
+
+const respuestaFilter = arreglo
+.filter(
+    (valorActual, indiceActual, ArregloCompleto)=>{
+        return valorActual.nota >= 12;
+    }
+);
+console.log('respuestaFilter', respuestaFilter);
+console.log('arreglo', arreglo);
+
+// SOME -> expresion
+// DEVUELVE BOOLEANO
+// Hay ALGUNA nota menor a nueve? SI NO
+// OR
+
+const respuestaSome =  arreglo
+    .some(
+        function (valorActual, indiceActual, rregloCompleto){
+            return valorActual.nota < 9;
+        }
+    );
+
+console.log('respuestaSome', respuestaSome);
+
+
+// EVERY -> expresion
+// DEVUELVE BOOLEANO
+// Todas las notas son mayores a 14? SI NO
+// AND
+
+const respuestaEvery = arreglo
+    .every(
+        function (valorActual, indiceActual, arregloCompleto){
+            return valorActual.nota > 14;
+        }
+    );
+
+console.log('respuestaEvery', respuestaEvery);
+
+// REDUCE           izq -> der
+// REDUCE RIGHT     der -> izq
+// 100 <3 puntos de vida
+// 100 -1 -2 -3 -4 -5 -5 -6 -7 =
+// 0 1 +2 +3 +6 +4 +8 +9 =
+// [1,2,3,4,5,6,7,8,9]
+
+const respuestaReduce =  arreglo
+    .reduce(
+        function (valorAcumulado,valorActual, indice, arreglo ){
+            return (valorAcumulado + valorActual.nota);
+        },
+        0 //Acumulador
+    );
+console.log('respuuestaReduce', respuestaReduce);
